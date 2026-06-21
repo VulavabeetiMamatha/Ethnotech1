81d99065d191c9ba63dcd5719271c61b061c5052
Vulavabeeti Mamatha <mamathacme18@gmail.com>
2026-06-21 18:26:35 +0530

    Add files via upload

---
+++ project/SmartMess/DBConnection.java
@@ -0,0 +1,14 @@
+package project.SmartMess;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection() throws Exception {
+
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/smartmess",
+                "root",
+                "Mammu@18");
+    }
+}
